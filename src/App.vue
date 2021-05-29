a963aa39fbba4059a427e54cf0b2a9970e935bae
Mujahid Anuar <mujahidfanuar@gmail.com>
2021-05-29 16:39:04 +0800

    Example: add number input to toggle size

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <input type="text" v-model="name" />
-  <Avatar :size="80" variant="bauhaus" :name="name" />
-  <Avatar :size="80" variant="beam" :name="name" />
-  <Avatar :size="80" variant="marble" :name="name" />
-  <Avatar :size="80" variant="pixel" :name="name" />
-  <Avatar :size="80" variant="ring" :name="name" />
-  <Avatar :size="80" variant="sunset" :name="name" />
+  <input type="number" v-model.number="size" />
+
+  <Avatar :size="size" variant="bauhaus" :name="name" />
+  <Avatar :size="size" variant="beam" :name="name" />
+  <Avatar :size="size" variant="marble" :name="name" />
+  <Avatar :size="size" variant="pixel" :name="name" />
+  <Avatar :size="size" variant="ring" :name="name" />
+  <Avatar :size="size" variant="sunset" :name="name" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import Avatar from "./components/Avatar.vue";
 
 export default defineComponent({
   name: "App",
-  data() {
+  setup() {
+    const name = ref("Clara Barton");
+    const size = ref(80);
+
     return {
-      name: "Clara Barton",
+      name,
+      size,
     };
   },
   components: {
