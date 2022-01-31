d5d0754000d7cf76ca492989d8fcd9e12dba3665
Mujahid Fakhrul Anuar <mujahidfanuar@gmail.com>
2022-01-31 19:24:52 +0800

    fix(Marble): Remove rx radius from mask

--- src/components/AvatarMarble.vue
+++ src/components/AvatarMarble.vue
@@ -22,31 +22,27 @@
       />
     </mask>
     <g mask="url(#mask__marble)">
-      <rect :width="SIZE" :height="SIZE" :rx="2" :fill="properties[0].color" />
+      <rect :width="SIZE" :height="SIZE" :fill="properties[0].color" />
       <path
         filter="url(#prefix__filter0_f)"
         d="M32.414 59.35L50.376 70.5H72.5v-71H33.728L26.5 13.381l19.057 27.08L32.414 59.35z"
         :fill="properties[1].color"
-        :transform="
-          `translate(${properties[1].translateX} ${
-            properties[1].translateY
-          }) rotate(${properties[1].rotate} ${SIZE / 2} ${SIZE / 2}) scale(${
-            properties[2].scale
-          })`
-        "
+        :transform="`translate(${properties[1].translateX} ${
+          properties[1].translateY
+        }) rotate(${properties[1].rotate} ${SIZE / 2} ${SIZE / 2}) scale(${
+          properties[2].scale
+        })`"
       />
       <path
         filter="url(#prefix__filter0_f)"
         class="mix-blend-overlay"
         d="M22.216 24L0 46.75l14.108 38.129L78 86l-3.081-59.276-22.378 4.005 12.972 20.186-23.35 27.395L22.215 24z"
         :fill="properties[2].color"
-        :transform="
-          `translate(${properties[2].translateX} ${
-            properties[2].translateY
-          }) rotate(${properties[2].rotate} ${SIZE / 2} ${SIZE / 2}) scale(${
-            properties[2].scale
-          })`
-        "
+        :transform="`translate(${properties[2].translateX} ${
+          properties[2].translateY
+        }) rotate(${properties[2].rotate} ${SIZE / 2} ${SIZE / 2}) scale(${
+          properties[2].scale
+        })`"
       />
     </g>
     <defs>
@@ -91,21 +87,6 @@ export default defineComponent({
     },
   },
   setup(props) {
-    const generateColors = (name: string, colors: string[]) => {
-      const numFromName = getNumber(name);
-      const range = colors && colors.length;
-
-      const elementsProperties = Array.from({ length: ELEMENTS }, (_, i) => ({
-        color: getRandomColor(numFromName + i, colors, range),
-        translateX: getUnit(numFromName * (i + 1), SIZE / 10, 1),
-        translateY: getUnit(numFromName * (i + 1), SIZE / 10, 2),
-        scale: 1.2 + getUnit(numFromName * (i + 1), SIZE / 20) / 10,
-        rotate: getUnit(numFromName * (i + 1), 360, 1),
-      }));
-
-      return elementsProperties;
-    };
-
     const properties = computed(() => generateColors(props.name, props.colors));
 
     return { properties, SIZE };
